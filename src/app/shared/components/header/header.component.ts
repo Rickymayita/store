2f73f761988d61da4f90b45928317f8cf88b7ad2
Ricardo Ariel Maya <rickymayita@hotmail.com>
2023-06-12 18:19:58 -0300

    using shoppingCartService

---
+++ src/app/shared/components/header/header.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { ShoppingCartService } from '../../services/shopping-cart.service';
+
+@Component({
+  selector: 'app-header',
+  template: `
+  <mat-toolbar color="primary">
+    <span>My Store</span>
+    {{quantity$ | async | json }} - Qantity
+    {{total$ | async | json }} - Total
+    {{cart$ | async | json }} - Cart
+  </mat-toolbar>
+  `,
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+  quantity$ = this.shoppingCartSvc.quantityAction$;
+  total$ = this.shoppingCartSvc.totalAction$;
+  cart$ = this.shoppingCartSvc.cartAction$;
+
+  constructor(private shoppingCartSvc: ShoppingCartService) {
+  }
+ }
